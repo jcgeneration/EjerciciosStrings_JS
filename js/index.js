ac3c1bb80865526e99a79db9d5ee823722461732
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-14 21:56:31 -0500

    Feat | Realice punto 4.3

--- js/index.js
+++ js/index.js
@@ -2,14 +2,25 @@
 let nombreCompleto;
 nombreCompleto=String(window.prompt("Ingresa tu nombre completo: "));
 /*> 4.2 Escribe en la consola del navegador su nombre y apellidos por separado*/
-nombreCompleto= nombreCompleto.split(" ");
-let apellidoM=nombreCompleto.pop();
-let apellidoP=nombreCompleto.pop();
-let tamano=nombreCompleto.length;
+let nombreCompletoSplit= nombreCompleto.split(" ");
+let apellidoM=nombreCompletoSplit.pop();
+let apellidoP=nombreCompletoSplit.pop();
+let tamano=nombreCompletoSplit.length;
 let nombre="";
 for(var i=0;i<tamano;i++){
-    nombre=nombre+" "+nombreCompleto.shift();
+    nombre=nombre+" "+nombreCompletoSplit.shift();
 }
 console.log("Su nombre es: "+nombre);
 console.log("Su apellido Paterno es: "+apellidoP);
 console.log("Su apellido Materno es: "+apellidoM);
+/*> 4.3 Escribe en la consola del navegador su Nombre Completo eliminando 
+los espacios en blanco al principio y al final*/
+espacioInicio= /^ /;
+espacioFinal = / $/;
+espaciosSeguidos = /[ ]+/g;
+nombreCompleto = nombreCompleto.replace (espaciosSeguidos," ");
+nombreCompleto = nombreCompleto.replace (espacioInicio,"");
+nombreCompleto = nombreCompleto.replace (espacioFinal,"");
+console.log(nombreCompleto);
+
+
