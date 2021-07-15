0ca88e1a97c50e9176adbae151fd4588df081c04
jjmartinez <juan.martinezu0@gmail.com>
2021-07-15 01:52:04 -0500

    Termine El ejercicio 4.7 y el bonus

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ console.log("Su apellido Materno es: "+apellidosM);
 
 
   //4.3 Escribe en la consola del navegador su Nombre Completo eliminando los espacios en blanco al principio y al final
+  
+  /*
+  El metodo replace() nos permite reemplazar una expresión por otra dentro de un string el cual 
+  acepta expresiones regulares o Regex en sus parametros. Con esto podemos tomar los espacios y removerlos.
 
+
+
+  \s: selecciona espacios en blanco, tabulaciones y saltos de linea.
+    +: en conjunto con \s sirve para tomar espacios consecutivos, de esta forma si 
+    hay 1 o mas espacios seguidos se eliminaran en conjunto.
+    g: permite repetir el proceso a lo largo del string o cadena, de otra forma solo se 
+    eliminaría el primer espacio encontrado 
+  
+  */
+
+
+  console.log(nombreCompleto.replace(/\s+/g, ''));
 
 
  // 4.4 Escribe en la consola del navegador true si el nombre inicia con una letra entre la A y la M, y escribe false de lo contrario
@@ -30,7 +46,7 @@ console.log("Su apellido Materno es: "+apellidosM);
           }
           return flag;
       }
-      console.log(condicion( nomCompleto ));
+      console.log(condicion( nombreCompleto ));
 
 //  4.5 Escribe en la consola del navegador el Apellido Paterno en Mayúsculas
       console.log(apellidosP.toUpperCase());
@@ -38,3 +54,16 @@ console.log("Su apellido Materno es: "+apellidosM);
       console.log(apellidosM.toLowerCase());
 // 4.7 Escribe en la consola del navegador el Nombre Completo en minúscula la inicial de cada parte y Mayúsculas el resto, por ejemplo con el nombre Jorge Pérez Ramos se debería escribir: jORGE pÉREZ rAMOS
 
+ console.log(nombreCompleto[0].charAt(0).toLowerCase() + nombreCompleto.split(" ")[0].slice(1) + " " +
+apellidosP.charAt(0).toLowerCase() + apellidosP.slice(1) + " " + apellidosM.charAt(0).toLowerCase() + apellidosM.slice(1) ); 
+
+//Bonus
+const invertirCadena = cadena => {
+    let nuevaCadena = "";
+    for (let x = cadena.length - 1; x >= 0; x--) {
+        nuevaCadena += cadena[x];
+    }
+    return nuevaCadena;
+}
+
+console.log(invertirCadena("juan martinez ulloa"));
