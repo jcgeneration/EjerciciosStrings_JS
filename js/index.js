a4b735adf5cf5f2aa42aefeac7979eba20a46591
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-14 22:26:00 -0500

    Feat | Realice punto 4.4

--- js/index.js
+++ js/index.js
@@ -7,8 +7,9 @@ let apellidoM=nombreCompletoSplit.pop();
 let apellidoP=nombreCompletoSplit.pop();
 let tamano=nombreCompletoSplit.length;
 let nombre="";
-for(var i=0;i<tamano;i++){
-    nombre=nombre+" "+nombreCompletoSplit.shift();
+for(i=0;i<tamano;i++){
+    nombre=nombre+nombreCompletoSplit.shift();
+    nombre=nombre+" ";
 }
 console.log("Su nombre es: "+nombre);
 console.log("Su apellido Paterno es: "+apellidoP);
@@ -22,5 +23,27 @@ nombreCompleto = nombreCompleto.replace (espaciosSeguidos," ");
 nombreCompleto = nombreCompleto.replace (espacioInicio,"");
 nombreCompleto = nombreCompleto.replace (espacioFinal,"");
 console.log(nombreCompleto);
+/*> 4.4 Escribe en la consola del navegador _true_ si el nombre 
+inicia con una letra entre la A y la M, y escribe _false_ de lo contrario*/
+nombreCompletoSplit= nombreCompleto.split(" ");
+apellidoM=nombreCompletoSplit.pop();
+apellidoP=nombreCompletoSplit.pop();
+tamano=nombreCompletoSplit.length;
+nombre="";
+
+for(i=0;i<tamano;i++){
+    nombre=nombre+nombreCompletoSplit.shift();
+    nombre=nombre+" ";
+}
+
+function verificarInicial(nombreI){
+    if(nombre[0]=='A' || nombre[0]=='B' || nombre[0]=='C' || nombre[0]=='D' || nombre[0]=='E' || nombre[0]=='F' || nombre[0]=='G' || nombre[0]=='H' || nombre[0]=='I' || nombre[0]=='J' || nombre[0]=='K' || nombre[0]=='L'|| nombre[0]=='M'){
+        console.log(true);
+    } else{
+        console.log(false);
+    }
+}
+
+verificarInicial(nombre);
 
 
