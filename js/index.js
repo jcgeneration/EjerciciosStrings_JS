76ef59fee5dcb67f274623f72411d7b2d0a90f3f
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-14 21:35:36 -0500

    Feat | Realice puntos 4.1 y 4.2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 /*> 4.1 Solicita al usuario su Nombre Completo mediante un window.prompt*/
-let nombre;
-nombre=String(window.prompt("Ingresa tu nombre completo: "));
+let nombreCompleto;
+nombreCompleto=String(window.prompt("Ingresa tu nombre completo: "));
+/*> 4.2 Escribe en la consola del navegador su nombre y apellidos por separado*/
+nombreCompleto= nombreCompleto.split(" ");
+let apellidoM=nombreCompleto.pop();
+let apellidoP=nombreCompleto.pop();
+let tamano=nombreCompleto.length;
+let nombre="";
+for(var i=0;i<tamano;i++){
+    nombre=nombre+" "+nombreCompleto.shift();
+}
+console.log("Su nombre es: "+nombre);
+console.log("Su apellido Paterno es: "+apellidoP);
+console.log("Su apellido Materno es: "+apellidoM);
